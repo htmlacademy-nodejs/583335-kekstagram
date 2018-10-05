b65f3601164ac293c782bc787f2c9e2edf159276
Arkhipov Valerii <vvarh@mail.ru>
2018-10-05 23:34:35 +0300

    +test generateEntity & Entity

---
+++ test/generateEntity.test.js
@@ -0,0 +1,68 @@
+'use strict';
+const MIN_NUMBER = 0; // мин число
+const MAX_NUMBER_SCALE = 100; // макс число уровня эффекта
+const MAX_NUMBER_LIKES = 1000; // макс число лайков
+const MAX_NUMBER_HASHTAGS = 5; // макс число #
+const MAX_LENGTH_DESCRIPTION = 140; // макс число #
+
+const assert = require(`assert`); // core module
+const genE = require(`../src/generateEntity.js`).execute(); // module func
+const E = genE(); // module Entity
+
+describe(`generateEntity`, () => {
+  describe(`#generateEntity()`, () => {
+    it(`Функция срабатывает без ошибок`, () => {
+      assert.doesNotThrow(genE, `missing foo`, `did not throw with expected message`);
+    });
+  });
+
+  describe(`#entity`, () => {
+
+    it(`Возвращает объект`, () => {
+      assert.equal(typeof E, `object`);
+    });
+
+    it(`url 600x600`, () => {
+      assert(E.url.indexOf(`http`) > -1);
+      assert(E.url.indexOf(`/600/`) > -1);
+    });
+
+    it(`scale: от 0 до 100`, () => {
+      assert(E.scale >= MIN_NUMBER && E.scale <= MAX_NUMBER_SCALE);
+    });
+
+    it(`effectт: 'none', 'chrome', 'sepia', 'marvin', 'phobos', 'heat'`, () => {
+      assert([`none`, `chrome`, `sepia`, `marvin`, `phobos`, `heat`].indexOf(E.effect) > -1);
+    });
+
+    it(`hashtags: 5`, () => {
+      assert(Array.isArray(E.hashtags));
+      assert(E.hashtags.length >= MIN_NUMBER && E.hashtags.length <= MAX_NUMBER_HASHTAGS);
+    });
+
+    it(`description: длина менее 140`, () => {
+      assert(E.description.length <= MAX_LENGTH_DESCRIPTION);
+    });
+
+    it(`likes: от 0 до 1000`, () => {
+      assert(E.likes >= MIN_NUMBER && E.likes <= MAX_NUMBER_LIKES);
+    });
+
+
+    it(`comments: isArray && comments: длина каждого элемента менее 140`, () => {
+      assert(Array.isArray(E.comments));
+
+      const validateLengthItem = (it) => {
+        assert(it.length <= MAX_LENGTH_DESCRIPTION);
+      };
+
+      E.comments.forEach((it) => validateLengthItem(it));
+    });
+
+    it(`date`, () => {
+
+    });
+
+  });
+
+});
