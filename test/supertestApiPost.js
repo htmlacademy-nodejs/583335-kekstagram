3ec97f2ea3cd150ea0008eab314225898f07eba9
Arkhipov Valerii <vvarh@mail.ru>
2018-10-22 23:26:00 +0300

    test post with validate data

--- test/supertestApiPost.js
+++ test/supertestApiPost.js
@@ -4,59 +4,116 @@ const assert = require(`assert`);
 const {app} = require(`../src/server`);
 
 const IMG_URL = `test/img/26.jpg`;
+const IMG_NAME = `26.jpg`;
+const POST_DESCRIPTION = `Самая красивая тачка на этой планете`;
+const POST_EFFECT = `chrome`;
+const POST_HASHTAGS = `#тачка #огонь #car #bmwX5`;
+const POST_SCALE = `100`;
 
-describe(`POST`, () => {
-  describe(`POST /api/posts/`, () => {
-    it(`send post as json`, async () => {
-      const sent = {
-        date: Date.now()
-      };
+describe(`POST /api/posts`, () => {
+  it(`send post as json`, async () => {
+    const sent = {
+      "filename": IMG_URL,
+      "description": POST_DESCRIPTION,
+      "effect": POST_EFFECT,
+      "hashtags": POST_HASHTAGS,
+      "scale": POST_SCALE,
+    };
 
-      const response = await supertest(app)
-        .post(`/api/posts`)
-        .send(sent)
-        .set(`Accept`, `application/json`)
-        .set(`Content-Type`, `application/json`)
-        .expect(200)
-        .expect(`Content-Type`, /json/);
+    const response = await supertest(app)
+      .post(`/api/posts`)
+      .send(sent)
+      .set(`Accept`, `application/json`)
+      .set(`Content-Type`, `application/json`)
+      .expect(200)
+      .expect(`Content-Type`, /json/);
+
+    const post = response.body;
+    assert.deepEqual(post, sent);
+  });
+
+  it(`send post as json with no url and get error`, async () => {
+    const sent = {
+      "filename": ``,
+      "description": POST_DESCRIPTION,
+      "effect": POST_EFFECT,
+      "hashtags": POST_HASHTAGS,
+      "scale": POST_SCALE,
+    };
+
+    await supertest(app)
+      .post(`/api/posts`)
+      .send(sent)
+      .set(`Accept`, `application/json`)
+      .set(`Content-Type`, `application/json`)
+      .expect(400);
+  });
+
+  it(`send post as multipart/form-data`, async () => {
+    const response = await supertest(app)
+      .post(`/api/posts`)
+      .field(`filename`, IMG_URL)
+      .field(`description`, POST_DESCRIPTION)
+      .field(`effect`, POST_EFFECT)
+      .field(`hashtags`, POST_HASHTAGS)
+      .field(`scale`, POST_SCALE)
+      .set(`Accept`, `application/json`)
+      .set(`Content-Type`, `multipart/form-data`)
+      .expect(200)
+      .expect(`Content-Type`, /json/);
 
 
-      const post = response.body;
-      assert.deepEqual(post, sent);
-    });
-    it(`send post as multipart/form-data`, async () => {
-      const date = Date.now();
+    const newPost = response.body;
+    assert.deepEqual(newPost,
+        {
+          filename: IMG_URL,
+          description: POST_DESCRIPTION,
+          effect: POST_EFFECT,
+          hashtags: POST_HASHTAGS,
+          scale: POST_SCALE,
+        }
+    );
+  });
 
-      const response = await supertest(app)
-        .post(`/api/posts`)
-        .field(`date`, date)
-        .set(`Accept`, `application/json`)
-        .set(`Content-Type`, `multipart/form-data`)
-        .expect(200)
-        .expect(`Content-Type`, /json/);
+  it(`send post with image as multipart/form-data`, async () => {
+    const response = await supertest(app)
+      .post(`/api/posts`)
+      .field(`filename`, IMG_URL)
+      .field(`description`, POST_DESCRIPTION)
+      .field(`effect`, POST_EFFECT)
+      .field(`hashtags`, POST_HASHTAGS)
+      .field(`scale`, POST_SCALE)
+      .attach(`image`, IMG_URL)
+      .set(`Accept`, `application/json`)
+      .set(`Content-Type`, `multipart/form-data`)
+      .expect(200)
+      .expect(`Content-Type`, /json/);
 
+    const post = response.body;
+    assert.deepEqual(post,
+        {
+          filename: IMG_URL,
+          description: POST_DESCRIPTION,
+          effect: POST_EFFECT,
+          hashtags: POST_HASHTAGS,
+          scale: POST_SCALE,
+          image: {
+            title: IMG_NAME
+          }
+        }
+    );
+  });
 
-      const post = response.body;
-      assert.deepEqual(post, {date});
-    });
-    it(`send post with pick as multipart/form-data`, async () => {
-      const date = Date.now();
-
-      const response = await supertest(app)
-        .post(`/api/posts`)
-        .field(`date`, date)
-        .attach(`url`, IMG_URL)
-        .set(`Accept`, `application/json`)
-        .set(`Content-Type`, `multipart/form-data`)
-        .expect(200)
-        .expect(`Content-Type`, /json/);
-
-
-      const post = response.body;
-      assert.deepEqual(post, {
-        date,
-        url: `26.jpg`
-      });
-    });
+  it(`send post as multipart/form-data with no date and get error`, async () => {
+    await supertest(app)
+      .post(`/api/posts`)
+      .field(`filename`, IMG_URL)
+      .field(`description`, POST_DESCRIPTION)
+      .field(`effects`, POST_EFFECT)
+      .field(`hashtags`, POST_HASHTAGS)
+      .field(`scale`, POST_SCALE)
+      .set(`Accept`, `application/json`)
+      .set(`Content-Type`, `multipart/form-data`)
+      .expect(400);
   });
 });
