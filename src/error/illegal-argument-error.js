7332060a5389352e8759fa98c97b50581848215a
Arkhipov Valerii <vvarh@mail.ru>
2018-11-06 00:32:21 +0300

    +logger

--- src/error/illegal-argument-error.js
+++ src/error/illegal-argument-error.js
@@ -1,8 +1,11 @@
 'use strict';
 
+const logger = require(`../logger`);
+
 module.exports = class IllegalArgumentError extends Error {
   constructor(message) {
     super(message);
     this.code = 400;
+    logger.error(`illegal-argument-error:  ${message}`, message);
   }
 };
